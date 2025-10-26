662def2503d6d3ef0351cc1bb957c7f919ee689e
molmosdev <hello@molmos.dev>
2025-10-26 20:14:15 +0100

    feat: add icons to buttons in home component and update imports

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Button, TranslatePipe } from '@basis-ng/primitives';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideCirclePlus, lucideCloudDownload } from '@ng-icons/lucide';
 
 @Component({
   selector: 's-home',
-  imports: [RouterLink, Button, TranslatePipe],
+  imports: [RouterLink, Button, TranslatePipe, NgIcon],
   template: `
     <button b-button routerLink="/create-event" class="b-variant-outlined">
+      <ng-icon name="lucideCirclePlus" size="16" />
       {{ 'home.create-event' | translate }}
     </button>
     <button b-button routerLink="/load-event" class="b-variant-outlined">
+      <ng-icon name="lucideCloudDownload" size="16" />
       {{ 'home.load-event' | translate }}
     </button>
   `,
   host: {
     class: 'flex flex-col gap-4 items-center justify-center h-full',
   },
+  providers: [provideIcons({ lucideCirclePlus, lucideCloudDownload })],
 })
 export class Home {}
