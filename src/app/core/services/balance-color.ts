d9bec9323b30e831cb19d96ab11be2246eb56302
molmosdev <hello@molmos.dev>
2025-10-29 02:02:55 +0100

    refactor: encapsulate state management in BalanceColor service

--- src/app/core/services/balance-color.ts
+++ src/app/core/services/balance-color.ts
@@ -1,8 +1,13 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BalanceColor {
-  state = signal<'positive' | 'negative' | 'zero'>('zero');
+  private _state = signal<'positive' | 'negative' | 'zero'>('zero');
+  state = computed(() => this._state());
+
+  set(newState: 'positive' | 'negative' | 'zero') {
+    this._state.set(newState);
+  }
 }
