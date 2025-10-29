66c6d04e55b7fcc5f4f9fc2675ab692cd506891b
molmosdev <hello@molmos.dev>
2025-10-29 02:03:17 +0100

    refactor: remove unused ParticipantsSelector component

--- src/app/pages/event/shared/participants-selector/participants-selector.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 's-participants-selector',
-  imports: [],
-  template: ` <p>participants-selector works!</p> `,
-  styles: ``,
-})
-export class ParticipantsSelector {}
