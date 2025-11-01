fc0f446f321258e82e1ed1d819d07d6c9b16fc35
molmosdev <hello@molmos.dev>
2025-11-01 19:41:40 +0100

    feat: add loading indicator with lucideLoader icon in load event component

--- src/app/pages/load-event/load-event.ts
+++ src/app/pages/load-event/load-event.ts
@@ -4,7 +4,7 @@ import { Button, Input, InputGroup, TranslatePipe, TranslationManager } from '@b
 import { Router, RouterLink } from '@angular/router';
 import { ApiEvents } from '../../core/services/api-events';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideArrowLeft, lucideCloudDownload, lucideTrash } from '@ng-icons/lucide';
+import { lucideArrowLeft, lucideCloudDownload, lucideLoader, lucideTrash } from '@ng-icons/lucide';
 
 @Component({
   selector: 's-load-event',
@@ -13,41 +13,52 @@ import { lucideArrowLeft, lucideCloudDownload, lucideTrash } from '@ng-icons/luc
     <button b-button routerLink="/home" class="b-variant-outlined b-squared absolute top-4 left-4">
       <ng-icon name="lucideArrowLeft" size="16" color="currentColor" />
     </button>
-    <b-input-group class="w-full max-w-xs">
-      <input
-        b-input
-        type="text"
-        [field]="form.eventId"
-        [placeholder]="'load-event.event-code' | translate"
+    @if (recentEvents.isLoading()) {
+      <ng-icon
+        name="lucideLoader"
+        size="23"
+        color="currentColor"
+        class="animate-spin absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
-      <button b-button class="b-size-sm b-squared b-variant-outlined" (click)="submitForm()">
-        <ng-icon name="lucideCloudDownload" size="16" color="currentColor" />
-      </button>
-    </b-input-group>
-    @if (eventIdError()) {
-      <span class="text-sm text-destructive dark:text-destructive-dark">{{ eventIdError() }}</span>
-    }
-    @if (recentEvents.hasValue() && recentEvents.value().length !== 0) {
-      @for (recentEvent of recentEvents.value(); track recentEvent.id) {
-        <div class="flex gap-2 w-full max-w-xs">
-          <button b-button class="b-variant-secondary flex-1" (click)="goToEvent(recentEvent.id)">
-            {{ recentEvent.name }}
-          </button>
-          <button
-            b-button
-            class="b-variant-secondary b-squared"
-            (click)="removeRecentEvent(recentEvent.id)"
-          >
-            <ng-icon name="lucideTrash" size="16" />
-          </button>
-        </div>
+    } @else {
+      <b-input-group class="w-full max-w-xs">
+        <input
+          b-input
+          type="text"
+          [field]="form.eventId"
+          [placeholder]="'load-event.event-code' | translate"
+        />
+        <button b-button class="b-size-sm b-squared b-variant-outlined" (click)="submitForm()">
+          <ng-icon name="lucideCloudDownload" size="16" color="currentColor" />
+        </button>
+      </b-input-group>
+      @if (eventIdError()) {
+        <span class="text-sm text-destructive dark:text-destructive-dark">{{
+          eventIdError()
+        }}</span>
+      }
+      @if (recentEvents.hasValue() && recentEvents.value().length !== 0) {
+        @for (recentEvent of recentEvents.value(); track recentEvent.id) {
+          <div class="flex gap-2 w-full max-w-xs">
+            <button b-button class="b-variant-secondary flex-1" (click)="goToEvent(recentEvent.id)">
+              {{ recentEvent.name }}
+            </button>
+            <button
+              b-button
+              class="b-variant-secondary b-squared"
+              (click)="removeRecentEvent(recentEvent.id)"
+            >
+              <ng-icon name="lucideTrash" size="16" />
+            </button>
+          </div>
+        }
       }
     }
   `,
   host: {
     class: 'flex flex-col gap-4 items-center justify-center h-full',
   },
-  providers: [provideIcons({ lucideArrowLeft, lucideCloudDownload, lucideTrash })],
+  providers: [provideIcons({ lucideArrowLeft, lucideCloudDownload, lucideTrash, lucideLoader })],
 })
 export class LoadEvent {
   router = inject(Router);
