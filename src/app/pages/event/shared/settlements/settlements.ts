8aa871a9024cb25b73cf14b1d05fcb29066d15f5
molmosdev <hello@molmos.dev>
2025-11-01 22:32:03 +0100

    fix: refactor settlements component for improved layout and translation handling

--- src/app/pages/event/shared/settlements/settlements.ts
+++ src/app/pages/event/shared/settlements/settlements.ts
@@ -1,33 +1,31 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { ISettlement } from '../../../../shared/interfaces/settlement.interface';
 import { IParticipant } from '../../../../shared/interfaces/participant.interface';
 import { CurrencyPipe } from '@angular/common';
-import { NgIcon, provideIcons } from '@ng-icons/core';
+import { TranslationManager } from '@basis-ng/primitives';
+import { provideIcons } from '@ng-icons/core';
 import { lucideArrowRight } from '@ng-icons/lucide';
 
 @Component({
   selector: 's-settlements',
-  imports: [CurrencyPipe, NgIcon],
+  imports: [CurrencyPipe],
   template: `
     @for (s of data(); track s.from + '-' + s.to) {
-      <div class="flex flex-col items-center gap-1">
-        {{ s.amount | currency: 'EUR' : 'symbol' : '1.2-2' : 'es' }}
-        <div class="flex gap-1 items-center">
-          {{ participantMap()[s.from] }}
-          <ng-icon name="lucideArrowRight" size="16" color="currentColor" />
-          {{ participantMap()[s.to] }}:
-        </div>
+      <div class="flex flex-col items-center gap-0.5">
+        {{ participantMap()[s.from] }} {{ mustText() }} {{ participantMap()[s.to] }}
+        <strong>{{ s.amount | currency: 'EUR' : 'symbol' : '1.2-2' : 'es' }}</strong>
       </div>
     }
   `,
   providers: [provideIcons({ lucideArrowRight })],
   host: {
-    class: 'flex flex-col gap-4 items-center justify-center',
+    class: 'flex flex-col gap-2 items-center justify-center',
   },
 })
 export class Settlements {
   data = input<ISettlement[]>();
   participants = input<IParticipant[]>();
+  private _translationManager = inject<TranslationManager>(TranslationManager);
   participantMap = computed(() => {
     const map: Record<string, string> = {};
     for (const p of this.participants() || []) {
@@ -35,4 +33,8 @@ export class Settlements {
     }
     return map;
   });
+
+  mustText() {
+    return this._translationManager.translate('event.settlements.must');
+  }
 }
