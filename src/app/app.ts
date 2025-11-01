99d38d9d1c324d29ffa1a3670a790687ca91a8be
molmosdev <hello@molmos.dev>
2025-11-01 18:49:54 +0100

    feat: replace header text with SVG icon for improved navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,8 +6,34 @@ import { BalanceColor } from './core/services/balance-color';
   selector: 's-root',
   imports: [RouterOutlet, RouterLink],
   template: `
-    <header class=" flex justify-center items-center h-8">
-      <a routerLink="/home" class="text-xl font-bold">sunio</a>
+    <header class=" flex justify-center items-center h-15">
+      <svg
+        class="cursor-pointer"
+        routerLink="/home"
+        width="30"
+        height="30"
+        viewBox="0 0 328 328"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <ellipse
+          cx="80.5"
+          cy="240"
+          rx="64.5"
+          ry="66"
+          transform="rotate(180 80.5 240)"
+          fill="currentColor"
+        />
+        <path
+          d="M81 174H181.697C201.176 174 215.49 192.275 210.823 211.187L192.493 285.469C189.517 297.527 178.726 306 166.305 306L81 306V174Z"
+          fill="currentColor"
+        />
+        <ellipse cx="247.5" cy="87" rx="64.5" ry="66" fill="currentColor" />
+        <path
+          d="M247 153H146.303C126.824 153 112.51 134.725 117.177 115.813L135.507 41.5313C138.483 29.4729 149.274 21 161.695 21L247 21V153Z"
+          fill="currentColor"
+        />
+      </svg>
     </header>
     <router-outlet />
   `,
